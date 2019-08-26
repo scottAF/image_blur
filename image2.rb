95dcbd4fbacc7ea1dc032fc245c2007f210b0656
Scott Ford <uofoduck14@gmail.com>
2019-08-26 17:19:26 +0000

    made image blur #2

---
+++ image2.rb
@@ -0,0 +1,45 @@
+class Image
+  attr_accessor :arr
+
+  def initialize(arr)
+      @arr = arr     
+  end
+
+  def output_image
+      @arr.each { |a| puts a.join("") }
+  end
+  
+  def blur
+      pixel = []
+      @arr.each_with_index do |row, rowIndex|
+          row.each_with_index do |col, colIndex|
+              pixel << [rowIndex, colIndex] if col == 1
+          end
+      end
+      
+      # pixel.each { |p| puts p.join("") }
+      
+      pixel.each do |num|
+          p = num[0]
+          q = num[1]
+          
+              @arr[p][q + 1] = 1 if q + 1 <= @arr[p].count - 1
+              @arr[p][q - 1] = 1 if q - 1 >= 0
+              @arr[p + 1][q] = 1 if p + 1 <= @arr.count - 1
+              @arr[p - 1][q] = 1 if p - 1 >= 0
+      end
+  
+  end
+#    
+end
+
+
+image = Image.new([
+[0, 0, 0, 0],
+[0, 1, 1, 0],
+[0, 0, 0, 1],
+[0, 0, 0, 0]
+])
+
+image.blur
+image.output_image
